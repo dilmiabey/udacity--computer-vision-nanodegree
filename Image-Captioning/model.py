fb436fac96a8120814e643454063b24dce50055c
Dilmi Abeytunga <dilmiabey@gmail.com>
2020-11-03 11:06:46 +0530

    First commit

---
+++ Image-Captioning/model.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+    
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super().__init__()
+        self.embedding_layer = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(input_size = embed_size, hidden_size = hidden_size, num_layers = num_layers,
+                            batch_first = True)
+        self.out = nn.Linear(hidden_size, vocab_size)
+    
+    def forward(self, features, captions):
+        captions = captions[:,:-1]
+        embed = self.embedding_layer(captions)
+        lstm_in = torch.cat((features.unsqueeze(1), embed), dim = 1)
+        output, _ = self.lstm(lstm_in)
+        out = self.out(output)
+        
+        return out
+        
+
+    def sample(self, inputs, states=None, max_len=20):
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        prediction = []
+        for i in range(max_len):
+            output, states = self.lstm(inputs, states)
+            output = output.squeeze(1)
+            out = self.out(output)
+            target = out.max(1)[1]
+            prediction.append(target.item())
+            inputs = self.embedding_layer(target).unsqueeze(1)
+            
+        return prediction
